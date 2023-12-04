a6a90356ad945ce00fd4b7a8c715bcfe193deac8
sandrine.brival <sandrine.brival@octo.com>
2023-12-04 17:08:57 +0100

    Version 2.0.1

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,19 +1,25 @@
 import { useState, useEffect } from 'react';
-import { optionsAuthent } from '../api/source_api';
 
 export const useFetch = (apiPath) => {
     const [data, setData] = useState([]);
     const baseUrl = `https://api.themoviedb.org/3${apiPath}?language=fr-FR`;
-    console.log(apiPath)
+    
 
     useEffect(()=> {
+        const optionsAuthent = {
+            method: 'GET',
+            headers: {
+              accept: 'application/json',
+              Authorization: process.env.REACT_APP_API_TOKEN,
+            }
+        }
         async function fetchMovies(){
         const response = await fetch(baseUrl, optionsAuthent);
         const json = await response.json();
         setData(json.results);
         }
         fetchMovies();
-    }, [apiPath]);
+    }, [apiPath, baseUrl]);
 
   return data 
 }
