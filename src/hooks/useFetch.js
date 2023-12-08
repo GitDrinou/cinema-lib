ce2e38af30a47d6fdd7c60db434ef9cb2e4ea3c5
sandrine.brival <sandrine.brival@octo.com>
2023-12-08 16:05:49 +0100

    Version 2.4.3

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 
 export const useFetch = (apiPath, queryString = "") => {
     const [data, setData] = useState([]);
-    const baseUrl = `https://api.themoviedb.org/3${apiPath}?api_key${process.env.REACT_APP_API_KEY}&language=fr-FR&query=${queryString}`;
+    const baseUrl = `https://api.themoviedb.org/3${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&language=fr-FR&query=${queryString}`;
     
 
     useEffect(()=> {
